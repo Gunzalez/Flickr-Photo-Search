e96beaaf4a862f91cbb03570e57eb6a29842bc8c
Segun Konibire <segun.konibire@springstudios.com>
2013-07-27 23:48:31 +0100

    Refactor to make more modular and reusable

--- js/flickrTest.js
+++ js/flickrTest.js
@@ -1,154 +1,117 @@
-// JavaScript Document
-var flickrTest = {	
-	utils: {
-		removeClass: function(el, rmvClass){
-			var curClass = el.className;
-			curClass = curClass.replace(' '+rmvClass, '');
-			curClass = curClass.replace(rmvClass+' ', '');
-			el.className = curClass;
-		},
-		addClass: function(el, newClass){
-			var curClass = el.className;
-			if(curClass.indexOf(newClass) == -1){
-				el.className = curClass + ' ' + newClass;
-			};
-		},
-		html: {
-			clearfloat: function(){
-				var clearFloat = document.createElement('div');	
-				clearFloat.className = 'cleft';
-				return clearFloat;
-			}
-		}	
-	},
-	
-	props: {
-		url: 'http://api.flickr.com/services/rest/?method=flickr.photos.search',
-		tokens: '&api_key=3adea58584c1edc262792b21cffffbcf&format=json&nojsoncallback=1',
-		photoArray: [],
-		itemsPerPage: 10,
-		pageCount: 0,
-		currentPage: 0
-	},
-	
-	methods:{
-		
-		// AJAX fetching data into array
-		fetch: function(){
-			var string = document.forms["frmFlickrSearch"]["searchTerm"].value;
-			var iPPel = document.getElementById('itemsPerPage');
-			flickrTest.props.itemsPerPage = iPPel.options[iPPel.selectedIndex].value;
-			if(string.length > 0){								
-				searchUrl = flickrTest.props.url + '&tags=' + string + '&text=' + string +  flickrTest.props.tokens;
-				flickrTest.props.photoArray = [];
-				var xmlhttp = new XMLHttpRequest();
-				xmlhttp.onreadystatechange = function(){
-					if (xmlhttp.readyState==4 && xmlhttp.status==200){		
-						var JSONObject = JSON.parse(xmlhttp.responseText);
-						if(JSONObject.stat == 'ok'){							
-							var length = JSONObject.photos.photo.length;
-							for(var i=0; i<length; i++){
-								var photo = JSONObject.photos.photo[i];
-								var imageSrc = 'http://farm'+ photo.farm +'.static.flickr.com/'+ photo.server +'/'+ photo.id +'_'+ photo.secret +'_q.jpg';
-								flickrTest.props.photoArray.push(imageSrc);
-							}
-							flickrTest.props.pageCount = parseInt(length / flickrTest.props.itemsPerPage);
-							if(length % flickrTest.props.itemsPerPage != 0){
-								flickrTest.props.pageCount = flickrTest.props.pageCount + 1
-							}
-							flickrTest.methods.displayPage(1);
-						}
-					}
-				}
-				xmlhttp.open("GET",searchUrl,true);
-				xmlhttp.send();				
-			}
-			return false;
-			
-		},
-		
-		displayPage: function(page){
-			
-			// display images for this page
-			flickrTest.props.currentPage = page;			
-			document.getElementById('photoBoard').innerHTML = '';
-			var upperBound = flickrTest.props.itemsPerPage * page;
-			var lowerBound = upperBound - flickrTest.props.itemsPerPage;
-			for(var i=lowerBound; i<upperBound; i++){
-				if(flickrTest.props.photoArray[i] != undefined){
-					var image = document.createElement('img');
-					image.src = flickrTest.props.photoArray[i];
-					image.width = '150';
-					image.height = '150';
-					document.getElementById('photoBoard').appendChild(image);
-				};
-			};
-			document.getElementById('photoBoard').appendChild(flickrTest.utils.html.clearfloat());
-			
-			// adjust paging information
-			document.getElementById('pageNo').innerHTML = flickrTest.props.currentPage + ' of ' + flickrTest.props.pageCount;
-			
-			//
-			document.getElementById('pagination').innerHTML = '';
-			for(var p=1; p<=flickrTest.props.pageCount; p++){
-				var pageLink = document.createElement('a');
-				pageLink.appendChild(document.createTextNode(p));
-				pageLink.href = '#'+p;
-				pageLink.onclick = function(e){
-					e.preventDefault();
-					var page = this.href.split('#')[1];
-					flickrTest.methods.displayPage(page);
-				}
-				if(p == flickrTest.props.currentPage){
-					pageLink.className = 'selected';
-				};
-				document.getElementById('pagination').appendChild(pageLink);
-			}
-			
-			// adjust controls
-			var controls = document.getElementById('controls').getElementsByTagName('a');
-			for(var c=0; c<controls.length; c++){
-				flickrTest.utils.removeClass(controls[c], 'disabled');
-				flickrTest.utils.removeClass(controls[c], 'displayNone');
-			};
-			if(page == 1){
-				flickrTest.utils.addClass(controls[0], 'disabled');	
-			};
-			if(page == flickrTest.props.pageCount){
-				flickrTest.utils.addClass(controls[1], 'disabled');	
-			};
-			
-		},
-		pageSwitch: function(e){
 
-			// Provides paging feature
-			e.preventDefault(); 
-			var control = e.target;
-			if(control.className.indexOf('disabled') == -1){
-				if(control.className.indexOf('prev') != -1){
-					flickrTest.methods.displayPage(parseInt(flickrTest.props.currentPage) - 1);
-				} else {
-					flickrTest.methods.displayPage(parseInt(flickrTest.props.currentPage) + 1);
-				};
-			}; 
-			
-		}
-	},
-	
-	init: function(){
-		
-		// Assign fetching event
-		document.getElementById('frmFlickrSearch').onsubmit = flickrTest.methods.fetch;
-		
-		// Previus and Next controls events
-		var controls = document.getElementById('controls').getElementsByTagName('a');
-		for(var c=0; c<controls.length; c++){
-			controls[c].onclick = flickrTest.methods.pageSwitch;	
+(function(){	
+	FlickrSearch = function(options){				
+		this.props = {
+			url: 'http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=3adea58584c1edc262792b21cffffbcf&format=json&nojsoncallback=1',
+			photoArray: [],
+			itemsPerPage: options.itemsPerPage || 10,
+			maxItems: options.maxItems || 100,
+			pageCount: 0,
+			currentPage: 0,
+			html: $('#flickrSearch')
 		};
 		
+		this.utils = {
+			html: {
+				clearfloat: function(){
+					return $clearFloat = $('<div class="cleft"></div>');
+				}
+			}	
+		};
+		
+		var that = this;
+		this.methods = {		
+			// pull in image via ajax, fill array
+			fetch: function(){								
+				var form = that.props.html.find('form');
+				var string = form.find('.searchTerm').val();
+				var url = that.props.url + '&per_page=' + that.props.maxItems + '&tags=' + string + '&text=' + string; 				
+				if($.trim(string).length > 0){
+					that.props.photoArray = [];
+					$.getJSON(url, function(data) {
+						$.each(data.photos.photo, function(key, val) {
+							var imgSrc = 'http://farm'+ val.farm +'.static.flickr.com/'+ val.server +'/'+ val.id +'_'+ val.secret +'_q.jpg';
+							that.props.photoArray.push(imgSrc);
+						});
+						var count = that.props.photoArray.length;
+						that.props.pageCount = parseInt(count / that.props.itemsPerPage);
+						if(count % that.props.itemsPerPage != 0){
+							that.props.pageCount = that.props.pageCount + 1
+						}
+						that.methods.displayPage(1);
+					});
+				}				
+			},
+			
+			displayPage: function(page){				
+				// display images for this page				
+				var photoBoard = that.props.html.find('.photoBoard');
+				photoBoard.empty();
+				that.props.currentPage = page;				
+				var upperBound = that.props.itemsPerPage * page;
+				var lowerBound = upperBound - that.props.itemsPerPage;
+											
+				for(var i=lowerBound; i<upperBound; i++){
+					if(that.props.photoArray[i] != undefined){
+						photoBoard.append($('<img src="'+that.props.photoArray[i]+'" width="150" height="150" />'));
+					};
+				};
+				photoBoard.append(that.utils.html.clearfloat());
+				
+				// adjust paging information
+				that.props.html.find('.pageNo').html('page ' + that.props.currentPage + ' of ' + that.props.pageCount);
+				
+				that.props.html.find('.pagination').empty();
+				for(var p=1; p<=that.props.pageCount; p++){		
+					var $link = $('<a href="#page'+p+'">'+p+'</a>');					
+					if(p == that.props.currentPage){
+						$link.addClass('selected');						
+					};
+					$link.on('click',function(e){
+						e.preventDefault()
+						that.methods.displayPage($(this).attr('href').split('#page')[1]);
+					})			
+					that.props.html.find('.pagination').append($link);
+				};			
+				
+				// adjust controls
+				that.props.html.find('.controls a').removeClass('displayNone').removeClass('disabled');
+				if(page == 1){
+					that.props.html.find('.controls .prev').addClass('disabled');
+				};
+				if(page == that.props.pageCount){
+					that.props.html.find('.controls .next').addClass('disabled');
+				};				
+			},
+			
+			switchPage: function($link){	 			
+				if(!$($link).hasClass('disabled')){
+					if($($link).hasClass('prev')){
+						that.methods.displayPage(parseInt(that.props.currentPage) - 1);
+					} else {
+						that.methods.displayPage(parseInt(that.props.currentPage) + 1);
+					};
+				};
+			}			
+		};		
+		
+		// assign fetching event
+		this.props.html.find('form').submit(function(){
+			that.methods.fetch();
+			return false;
+		});
+		
+		// previous and Next controls events
+		this.props.html.find('.controls a').on('click', function(e){
+			e.preventDefault()
+			that.methods.switchPage(this);
+		});
 	}	
-}
-
-window.onload = function () {
-	flickrTest.init();
-};
+	
+	$(document).ready(function(){		
+		var flickrSearch = new FlickrSearch({
+			itemsPerPage: 25, 	// set items per page
+			maxItems: 125		// set total number of items to fetch
+		});		
+	});		
+}())
