6b5f8b0b68573998be25fa59ffd0846f32b2ca90
Segun Konibire <segun.konibire@springstudios.com>
2013-07-28 11:27:01 +0100

    Uses delegation for more efficient use of memory

--- js/flickrTest.js
+++ js/flickrTest.js
@@ -9,7 +9,7 @@
 			pageCount: 0,
 			currentPage: 0,
 			html: $('#flickrSearch')
-		};
+		}
 		
 		this.utils = {
 			html: {
@@ -17,17 +17,17 @@
 					return $clearFloat = $('<div class="cleft"></div>');
 				}
 			}	
-		};
+		}
 		
 		var that = this;
 		this.methods = {		
 			// pull in data via ajax, fill up the array
 			fetch: function(){								
 				var form = that.props.html.find('form');
-				var string = form.find('.searchTerm').val();
+				var string = $.trim(form.find('.searchTerm').val());
 				var params = { 'per_page': that.props.maxItems, 'tags': string, 'text': string }				
 				var url = that.props.url + '&' + $.param(params);				
-				if($.trim(string).length > 0){
+				if(string.length > 0){
 					that.props.photoArray = [];
 					$.getJSON(url, function(data) {
 						$.each(data.photos.photo, function(key, val) {
@@ -39,7 +39,9 @@
 						if(count % that.props.itemsPerPage != 0){
 							that.props.pageCount = that.props.pageCount + 1
 						}
-						that.methods.displayPage(1); // Display page one of results
+						if(that.props.pageCount > 0){
+							that.methods.displayPage(1); // Display page one of results	
+						}						
 					});
 				}				
 			},
@@ -48,41 +50,46 @@
 				// display images for this page				
 				var photoBoard = that.props.html.find('.photoBoard');
 				photoBoard.empty();
-				that.props.currentPage = page;				
+				
+				// set current page
+				that.props.currentPage = page;
+				
+				// set start and end points of the loop				
 				var upperBound = that.props.itemsPerPage * page;
-				var lowerBound = upperBound - that.props.itemsPerPage;
-											
+				var lowerBound = upperBound - that.props.itemsPerPage;											
 				for(var i=lowerBound; i<upperBound; i++){
 					if(that.props.photoArray[i] != undefined){
 						photoBoard.append($('<img src="'+that.props.photoArray[i]+'" width="150" height="150" />'));
-					};
-				};
+					}
+				}
 				photoBoard.append(that.utils.html.clearfloat());
 				
-				// adjust paging information
-				that.props.html.find('.pageNo').html('page ' + that.props.currentPage + ' of ' + that.props.pageCount);
+				// adjust paging info 
+				that.props.html.find('.pageNo').html(that.props.photoArray.length + ' images | page ' + that.props.currentPage + ' of ' + that.props.pageCount);
 				
-				that.props.html.find('.pagination').empty();
+				// adjust pagination info
+				var $pagination = that.props.html.find('.pagination');
+				$pagination.empty();
 				for(var p=1; p<=that.props.pageCount; p++){		
 					var $link = $('<a href="#page'+p+'">'+p+'</a>');					
 					if(p == that.props.currentPage){
 						$link.addClass('selected');						
-					};
-					$link.on('click',function(e){
-						e.preventDefault()
-						that.methods.displayPage($(this).attr('href').split('#page')[1]);
-					})			
-					that.props.html.find('.pagination').append($link);
-				};			
+					}			
+					$pagination.append($link);
+				};				
+				$pagination.on('click', 'a', function(e){
+					e.preventDefault();
+					that.methods.displayPage($(this).attr('href').split('#page')[1]);
+				});
 				
 				// adjust controls
 				that.props.html.find('.controls a').removeClass('displayNone disabled');
-				if(page == 1){
+				if(parseInt(page) === 1){
 					that.props.html.find('.controls .prev').addClass('disabled');
-				};
-				if(page == that.props.pageCount){
+				}
+				if(parseInt(page) === parseInt(that.props.pageCount)){
 					that.props.html.find('.controls .next').addClass('disabled');
-				};				
+				}				
 			},
 			
 			switchPage: function($link){	 			
@@ -91,8 +98,8 @@
 						that.methods.displayPage(parseInt(that.props.currentPage) - 1);
 					} else {
 						that.methods.displayPage(parseInt(that.props.currentPage) + 1);
-					};
-				};
+					}
+				}
 			}			
 		};		
 		
@@ -113,7 +120,7 @@
 				
 		flickrSearch = new FlickrSearch({
 			itemsPerPage: 25, 	// set items per page
-			maxItems: 125		// set total number of items to fetch
+			maxItems: 124		// set total number of items to fetch
 		});	
 			
 	});		
