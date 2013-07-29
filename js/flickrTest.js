d2a8a8ade781ca85bb1eeb2efedf7d605af613b3
Segun Konibire <segun.konibire@springstudios.com>
2013-07-29 02:01:34 +0100

    Refactors to stop re-writing the DOM

--- js/flickrTest.js
+++ js/flickrTest.js
@@ -22,41 +22,82 @@
 		var that = this;
 		this.methods = {		
 			// pull in data via ajax, fill up the array
-			fetch: function(){								
+			fetch: function(){	
+										
 				var form = that.props.html.find('form');
 				var string = $.trim(form.find('.searchTerm').val());
 				var params = { 'per_page': that.props.maxItems, 'tags': string, 'text': string }				
-				var url = that.props.url + '&' + $.param(params);				
-				if(string.length > 0){
+				var url = that.props.url + '&' + $.param(params);
+								
+				if(string.length > 0){	
+								
 					that.props.photoArray = [];
 					$.getJSON(url, function(data) {
+						
 						$.each(data.photos.photo, function(key, val) {
 							var imgSrc = 'http://farm'+ val.farm +'.static.flickr.com/'+ val.server +'/'+ val.id +'_'+ val.secret +'_q.jpg';
 							that.props.photoArray.push(imgSrc);
 						});
+						
 						var count = that.props.photoArray.length;
 						that.props.pageCount = parseInt(count / that.props.itemsPerPage);
 						if(count % that.props.itemsPerPage != 0){
 							that.props.pageCount = that.props.pageCount + 1
 						}
+								
 						if(that.props.pageCount > 0){
-							that.methods.displayPage(1); // Display page one of results	
-						}						
+																				
+							// create pagination
+							var $pagination = that.props.html.find('.pagination');
+							$pagination.empty();
+							for(var p=1; p<=that.props.pageCount; p++){		
+								var $link = $('<a href="#page'+p+'">'+p+'</a>');			
+								$pagination.append($link);
+							};				
+							$pagination.on('click', 'a', function(e){
+								e.preventDefault();
+								that.methods.displayPage($(this).attr('href').split('#page')[1]);
+							});	
+							
+							// set paging info 
+							var $pageNo = that.props.html.find('.pageNo');
+							$pageNo.find('.total-images').html(that.props.photoArray.length);
+							$pageNo.find('.current-page').html(that.props.currentPage);
+							$pageNo.find('.total-pages').html(that.props.pageCount);							
+		
+							// set previous and next controls events
+							var $controls = that.props.html.find('.controls');
+							$controls.find('a').on('click', function(e){
+								e.preventDefault()
+								that.methods.switchPage(this);
+							});
+							
+							// display controls
+							$controls.removeClass('displayNone');
+							
+							// display page 1 of results						
+							that.methods.displayPage(1); 
+								
+						}
+												
 					});
-				}				
+				}
+								
 			},
 			
-			displayPage: function(page){				
-				// display images for this page				
-				var photoBoard = that.props.html.find('.photoBoard');
-				photoBoard.empty();
-				
+			displayPage: function(page){
+								
 				// set current page
 				that.props.currentPage = page;
 				
+				// display images for this page				
+				var photoBoard = that.props.html.find('.photoBoard');	
+				photoBoard.empty();				
+				
 				// set start and end points of the loop				
 				var upperBound = that.props.itemsPerPage * page;
-				var lowerBound = upperBound - that.props.itemsPerPage;											
+				var lowerBound = upperBound - that.props.itemsPerPage;
+														
 				for(var i=lowerBound; i<upperBound; i++){
 					if(that.props.photoArray[i] != undefined){
 						photoBoard.append($('<img src="'+that.props.photoArray[i]+'" width="150" height="150" />'));
@@ -65,30 +106,22 @@
 				photoBoard.append(that.utils.html.clearfloat());
 				
 				// adjust paging info 
-				that.props.html.find('.pageNo').html(that.props.photoArray.length + ' images | page ' + that.props.currentPage + ' of ' + that.props.pageCount);
+				var $pageNo = that.props.html.find('.pageNo');
+				$pageNo.find('.current-page').html(that.props.currentPage);
 				
-				// adjust pagination info
+				// adjust pagination
 				var $pagination = that.props.html.find('.pagination');
-				$pagination.empty();
-				for(var p=1; p<=that.props.pageCount; p++){		
-					var $link = $('<a href="#page'+p+'">'+p+'</a>');					
-					if(p == that.props.currentPage){
-						$link.addClass('selected');						
-					}			
-					$pagination.append($link);
-				};				
-				$pagination.on('click', 'a', function(e){
-					e.preventDefault();
-					that.methods.displayPage($(this).attr('href').split('#page')[1]);
-				});
-				
+				$pagination.find('.selected').removeClass('selected');
+				$pagination.find('a:eq('+(page-1)+')').addClass('selected');
+								
 				// adjust controls
-				that.props.html.find('.controls a').removeClass('displayNone disabled');
+				var $controls = that.props.html.find('.controls') 
+				$controls.find('a').removeClass('disabled');
 				if(parseInt(page) === 1){
-					that.props.html.find('.controls .prev').addClass('disabled');
+					$controls.find('.prev').addClass('disabled');
 				}
 				if(parseInt(page) === parseInt(that.props.pageCount)){
-					that.props.html.find('.controls .next').addClass('disabled');
+					$controls.find('.next').addClass('disabled');
 				}				
 			},
 			
@@ -108,17 +141,11 @@
 			that.methods.fetch();
 			return false;
 		});
-		
-		// previous and Next controls events
-		this.props.html.find('.controls a').on('click', function(e){
-			e.preventDefault()
-			that.methods.switchPage(this);
-		});
 	}	
 	
 	$(document).ready(function(){
 				
-		flickrSearch = new FlickrSearch({
+		var flickrSearch = new FlickrSearch({
 			itemsPerPage: 25, 	// set items per page
 			maxItems: 124		// set total number of items to fetch
 		});	
